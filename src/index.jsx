774a888537bd8c11b5b26056c8af2f326181d053
Bekele Etsub <bekeleetsub857-cloud@example.com>
2025-12-07 14:58:37 +0300

    Initial commit: Etsub Shop

--- src/index.jsx
+++ src/index.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { ProductProvider } from "./context/ProductContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ProductProvider>
-      <HashRouter>
+      <BrowserRouter basename="/etsub-shop">  {/* ← Change to etsub-shop */}
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </ProductProvider>
   </React.StrictMode>
 );
