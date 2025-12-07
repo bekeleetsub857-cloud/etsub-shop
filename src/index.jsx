3576660cb0a31a203f6addbbb5f0dc5163829ad3
bekeleetsub857-cloud <bekeleetsub857@gmail.com>
2025-12-07 15:26:38 +0300

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { HashRouter } from "react-router-dom";  // ← Changed to HashRouter
+import { HashRouter } from "react-router-dom";
 import { ProductProvider } from "./context/ProductContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ProductProvider>
-      <HashRouter>  {/* ← No basename needed */}
+      <HashRouter>
         <App />
       </HashRouter>
     </ProductProvider>
