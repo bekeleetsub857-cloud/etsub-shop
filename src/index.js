440c7e4a463989bbbf070eeaff207f4890f147ff
bekeleetsub857-cloud <bekeleetsub857@gmail.com>
2025-12-08 10:58:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css"; 
 import App from "./App"; 
 import { HashRouter } from "react-router-dom"; 
-import { ProductProvider } from "./context/ProductContext"; 
- 
+
 const root = ReactDOM.createRoot(document.getElementById("root")); 
 root.render( 
   <React.StrictMode> 
-    <ProductProvider> 
-      <HashRouter> 
-        <App /> 
-      </HashRouter> 
-    </ProductProvider> 
+    <HashRouter> 
+      <App /> 
+    </HashRouter> 
   </React.StrictMode> 
+);
